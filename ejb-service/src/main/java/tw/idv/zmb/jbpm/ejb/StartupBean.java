a7c1a825ad9d3cec449a4099b553d6eed8e11a9b
Zombie <chuanghs@gmail.com>
2017-02-03 14:46:32 +0800

    jbpm ejb service project

---
+++ ejb-service/src/main/java/tw/idv/zmb/jbpm/ejb/StartupBean.java
@@ -0,0 +1,13 @@
+package com.droolsjbpm.example;
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+
+@Singleton
+@Startup
+public class StartupBean {
+    @PostConstruct
+    public void init() {
+	System.setProperty("org.jbpm.ht.callback", "jaas");
+    }
+};
